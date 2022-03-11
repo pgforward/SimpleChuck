a4fb39641b037090d1e76d4968c939e3b59c48d8
Peter Forward <petejenny@hotmail.com>
2022-03-10 18:51:16 -0800

    Initial Commit

---
+++ SimpleChuck/SimpleChuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChuckApp.swift
+//  SimpleChuck
+//
+//  Created by Peter Forward on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
